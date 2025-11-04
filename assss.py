ab28d5cdbbd312d798937853f82daf6e437c6524
yem5 <1284x1485@gmail.com>
2025-11-04 14:46:31 -0500

    calcu

---
+++ assss.py
@@ -0,0 +1,5 @@
+num = int(input("Ingrese un numero"))
+num2 = int(input("Ingrese segundo nume"))
+sum = num + num2
+print(f"Total {sum} holmasdnasidjnasjdksa ")
+print("total", sum ,"hola")
---
+++ funcidch.py
@@ -0,0 +1,21 @@
+
+def multip():
+    x = int(input("ingresa num"))
+    y = int(input("ingresa num"))
+    r = x * y
+
+    print(f"resultado  {r}")
+
+multip()
+
+
+
+"""
+def multip(x,y):
+    r = x * y
+    print(f"resultado  {r}")
+
+multip(2,2)
+"""
+
+    
